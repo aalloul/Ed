40eaffc3e5e6846600d43a97ff7fad7883f6b146
Pavlik <paulcodiny@gmail.com>
2017-12-26 01:49:47 +0100

    Added Google Analytics

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Home from './routes/Home';
+
 import Footer from './containers/Footer/Footer';
 
 export default props => (
@@ -15,6 +17,18 @@ export default props => (
 
       <Footer />
     </div>
-  </React.Fragment>
 
-)
+    <script dangerouslySetInnerHTML={{
+      __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-93678494-4', 'auto');
+        ga('send', 'pageview');
+      `}}
+    >
+    </script>
+  </React.Fragment>
+);
