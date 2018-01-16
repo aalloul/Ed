fafb71d2ae7f11a98376a44faec8b40e680ba263
Pavlik <paulcodiny@gmail.com>
2018-01-16 23:35:22 +0100

    Added Growity code to the website

--- Landing/src/containers/Header/Header.js
+++ Landing/src/containers/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import glamorous from 'glamorous';
 
 const Header = glamorous.header({
@@ -27,16 +27,31 @@ const MenuItem = glamorous.a({
 })
 
 export default () => (
-  <Header>
-    <Logo>
-      <img src="/img/smail_logo.png" width="100"/>
-    </Logo>
-    <Menu>
-      <MenuItem>Top</MenuItem>
-      <MenuItem>How it works</MenuItem>
-      <MenuItem>Reviews</MenuItem>
-      <MenuItem>Pricing</MenuItem>
-      <MenuItem>Contact</MenuItem>
-    </Menu>
-  </Header>
+  <Fragment>
+    <script dangerouslySetInnerHTML={{
+      __html: `
+        <script type="text/javascript">
+          (function(d) {
+            var g = d.createElement('script');
+            g.type = 'text/javascript';
+            g.src = d.location.protocol + '//s.growity.me/cj76v2o9cwapc0131tqxxfn57.js';
+            d.head.appendChild(g);
+          })(document);
+        </script>
+      `
+    }}>
+    </script>
+    <Header>
+      <Logo>
+        <img src="/img/smail_logo.png" width="100" />
+      </Logo>
+      <Menu>
+        <MenuItem>Top</MenuItem>
+        <MenuItem>How it works</MenuItem>
+        <MenuItem>Reviews</MenuItem>
+        <MenuItem>Pricing</MenuItem>
+        <MenuItem>Contact</MenuItem>
+      </Menu>
+    </Header>
+  </Fragment>
 );
