79868c994c6a9af7aed1d3addc9e5db2b76f059a
Anastasia Korzhikova <as.korzhikova@gmail.com>
2018-01-24 08:25:20 +0100

    Add Burger Menu

--- landing/src/App.js
+++ landing/src/App.js
@@ -42,7 +42,7 @@ export default props => (
     }}>
     </script>
 
-    <div className="content">
+    <div className="content" style={{overflowX: 'hidden'}}>
       <Header />
       <Router
         basename={props.basename}
