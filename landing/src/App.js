06619a6b6ba59b2704db490a36038092471a77f6
Pavlik <paulcodiny@gmail.com>
2018-02-12 00:32:47 +0100

    Added yandex.metrika

--- landing/src/App.js
+++ landing/src/App.js
@@ -20,10 +20,6 @@ export default props => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content={props.description} />
 
-    <style dangerouslySetInnerHTML={{
-    __html: 'body { margin: 0; padding: 0; font-family: "Lato", sans-serif; font-size: 15px; background: #f5f2f1; color: #714f4f; -webkit-font-smoothing: antialiased; }'
-    }} />
-
     <Favicons />
 
     <style dangerouslySetInnerHTML={{
@@ -132,6 +128,31 @@ export default props => (
 
         ga('create', 'UA-93678494-4', 'auto');
         ga('send', 'pageview');
+
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter47652088 = new Ya.Metrika({
+                        id:47652088,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
       `}}
     >
     </script>
