98f7f6685036dd0267907be4649117347bbd605c
Karafizi Arthur <karafizi.artur@gmail.com>
2018-02-19 18:42:05 +0300

    Add IssuesSection

--- landing/src/routes/Home.js
+++ landing/src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import HeroSection from '../containers/HeroSection/HeroSection';
 import HiwSection from '../containers/HiwSection/HiwSection';
+import IssuesSection from '../containers/IssuesSection/IssuesSection';
 // import PricingSection from '../containers/PricingSection/PricingSection';
 // import ReviewsSection from '../containers/ReviewsSection/ReviewsSection';
 
 export default () => (
   <React.Fragment>
     <HeroSection />
+    <IssuesSection />
     <HiwSection />
     { /* <PricingSection /> */ }
     { /* <ReviewsSection /> */ }
